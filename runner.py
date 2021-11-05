755c16bd5acc6328909be3f02ee4d1b20fed77ca
Przemek <kainoj@users.noreply.github.com>
2021-11-05 16:34:46 +0900

    Support to choose GPU index

--- runner.py
+++ runner.py
@@ -7,7 +7,7 @@ from pathlib import Path
 TASKS = ['stsb', 'mrpc', 'cola', 'wnli', 'sst2', 'qnli', 'rte', 'qqp', 'mnli']
 
 
-def run_glues(model_path):
+def run_glues(model_path, gpu_idx: str = '0'):
     model_path = Path(model_path)
 
     if not model_path.exists():
@@ -21,7 +21,7 @@ def run_glues(model_path):
         output_dir = model_path / "glue" / task
 
         cmd = f"""\
-        CUDA_VISIBLE_DEVICES=0 python run_glue.py \
+        CUDA_VISIBLE_DEVICES={gpu_idx} python run_glue.py \
             --model_name_or_path  {model_path} \
             --task_name {task} \
             --do_train \
@@ -39,8 +39,15 @@ def run_glues(model_path):
 def main():
     parser = argparse.ArgumentParser()
     parser.add_argument('model_path', help='Path to pre-trained model.')
+    parser.add_argument(
+        '--gpu_idx',
+        help='GPU index, as used in CUDA_VISIBLE_DEVICES.'
+    )
+
     args = parser.parse_args()
-    run_glues(args.model_path)
+    run_glues(args.model_path, gpu_idx=args.gpu_idx)
+
+    print(f"Finished:\n{args.model_name}/glue")
 
 
 if __name__ == '__main__':
